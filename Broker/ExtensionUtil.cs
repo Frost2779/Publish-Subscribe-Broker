594907cec6cddb979afb4556bfe7ba3644586679
Michael S <Frost2779@users.noreply.github.com>
2020-06-24 14:05:30 -0500

    Small amount of cleanup work

---
+++ Broker/ExtensionUtil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Broker {
+    public static class ExtensionUtil {
+
+        public static bool EqualsIgnoreCase(this string caller, string toCompare) {
+            return caller.ToLower().Equals(toCompare.ToLower());
+        }    
+    }
+}
--- Broker/PubSubBroker.cs
+++ Broker/PubSubBroker.cs
@@ -4,12 +4,16 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace Broker {
     public class PubSubBroker {
 
         private bool _isBrokerAlive = false;
         private TcpListener _brokerTcpListener = null;
+
+		private ConcurrentDictionary<string, string> _topicDictionary; //Types are placeholder at the moment
 
         public void Start() {
             SpoolTcpListener();
@@ -33,12 +37,12 @@ namespace Broker {
                     string idPacket = connectionStream.ReadAllDataAsString();
 
                     if (idPacket == PacketHandler.I_AM_PUBLISHER_PACKET) {
-                        Thread publisherThread = new Thread(() => HandlePublisher(connectionStream, clientConnectionID));
+                        Thread publisherThread = new Thread(() => HandlePublisherThread(connectionStream, clientConnectionID));
                         publisherThread.Start();
                         Console.WriteLine($"[Publisher '{clientConnectionID}'] Connection made");
                     }
                     else if (idPacket == PacketHandler.I_AM_SUBSCRIBER_PACKET) {
-                        Thread publisherThread = new Thread(() => HandleSubscriber(connectionStream, clientConnectionID));
+                        Thread publisherThread = new Thread(() => HandleSubscriberThread(connectionStream, clientConnectionID));
                         publisherThread.Start();
                         Console.WriteLine($"[Subscriber '{clientConnectionID}'] Connection made");
                     }
@@ -51,34 +55,51 @@ namespace Broker {
                 Console.WriteLine(e.StackTrace);
                 _isBrokerAlive = false;
             }
+
         }
 
         private void StartUserInputLoop() {
             while (_isBrokerAlive) {
+                string userInput = Console.ReadLine();
 
+                if (userInput.EqualsIgnoreCase("Quit")) {
+                    BeginBrokerShutdown();
+                }
+                else if (userInput.EqualsIgnoreCase("List Topics")) {
+
+                }
+                else if (userInput.EqualsIgnoreCase("List Connections")) { 
+                
+                }
             }
         }
 
-        private void HandlePublisher(NetworkStream pubNetworkStream, Guid connectionID) {
+        private void BeginBrokerShutdown() {
+            _isBrokerAlive = false;
+        }
+
+        private void HandlePublisherThread(NetworkStream pubNetworkStream, Guid connectionID) {
             try {
-                pubNetworkStream.Write($"Connection made with broker. Given id '{connectionID}'".AsASCIIBytes());
+                SendConnectionConfirmation(pubNetworkStream, connectionID);
 
                 while (_isBrokerAlive) {
                     string packet = pubNetworkStream.ReadAllDataAsString();
 
                 }
+                SendBrokerShutdownMessage(pubNetworkStream);
             }
             catch (Exception e) { HandleConnectionException(e, connectionID); }
         }
 
-        private void HandleSubscriber(NetworkStream subetworkStream, Guid connectionID) {
+        private void HandleSubscriberThread(NetworkStream subNetworkStream, Guid connectionID) {
             try {
-                subetworkStream.Write($"Connection made with broker. Given id '{connectionID}'".AsASCIIBytes());
+                SendConnectionConfirmation(subNetworkStream, connectionID);
 
                 while (_isBrokerAlive) {
-                    string packet = subetworkStream.ReadAllDataAsString();
+                    string packet = subNetworkStream.ReadAllDataAsString();
 
                 }
+                SendBrokerShutdownMessage(subNetworkStream);
             }
             catch (Exception e) { HandleConnectionException(e, connectionID); }
         }
@@ -86,5 +107,12 @@ namespace Broker {
         private void HandleConnectionException(Exception e, Guid id) {
             Console.WriteLine($"Connection with client of ID '{id}' has dropped with the following error: \n {e.StackTrace}");
         }
+        private void SendConnectionConfirmation(NetworkStream stream, Guid connectionID) {
+            stream.Write($"Connection made with broker. Given id '{connectionID}'".AsASCIIBytes());
+        }
+        private void SendBrokerShutdownMessage(NetworkStream stream) {
+            stream.Write("Broker is shutting down. Connection will be dropped.".AsASCIIBytes());
+        }
+
     }
 }
